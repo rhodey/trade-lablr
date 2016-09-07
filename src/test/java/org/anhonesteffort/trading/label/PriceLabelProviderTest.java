730b76abdea018a53fd9045ebec96eea31a766d6
Rhodey Orbits <rhodey@anhonesteffort.org>
2016-09-06 21:09:24 -0500

    test price label going backwards in time

--- src/test/java/org/anhonesteffort/trading/label/PriceLabelProviderTest.java
+++ src/test/java/org/anhonesteffort/trading/label/PriceLabelProviderTest.java
@@ -27,13 +27,17 @@ public class PriceLabelProviderTest {
     return new OrderEvent(OrderEvent.Type.TAKE, timeMs, -1l, "idk", Order.Side.ASK, price, -1l);
   }
 
+  private OrderEvent mockOpen(long timeMs, long price) {
+    return new OrderEvent(OrderEvent.Type.OPEN, timeMs, -1l, "idk", Order.Side.ASK, price, -1l);
+  }
+
   @Test
   public void test1() {
     final LabelProvider LABELER = new PriceLabelProvider(100l);
 
     LABELER.initEventCount(5);
     LABELER.indexEvent(0, mockTake(0l,   1l));
-    LABELER.indexEvent(1, mockTake(25l,  13l));
+    LABELER.indexEvent(1, mockOpen(25l,  13l));
     LABELER.indexEvent(2, mockTake(50l,  133l));
     LABELER.indexEvent(3, mockTake(75l,  1337l));
     LABELER.indexEvent(4, mockTake(100l, 31337l));
@@ -47,19 +51,19 @@ public class PriceLabelProviderTest {
 
   @Test
   public void test2() {
-    final LabelProvider LABELER = new PriceLabelProvider(10l);
+    final LabelProvider LABELER = new PriceLabelProvider(-10l);
 
     LABELER.initEventCount(5);
     LABELER.indexEvent(0, mockTake(0l,   1l));
-    LABELER.indexEvent(1, mockTake(25l,  13l));
+    LABELER.indexEvent(1, mockOpen(25l,  13l));
     LABELER.indexEvent(2, mockTake(30l,  133l));
     LABELER.indexEvent(3, mockTake(39l,  1337l));
     LABELER.indexEvent(4, mockTake(49l, 31337l));
 
     assert LABELER.labelFor(0).getValue() == 1l;
-    assert LABELER.labelFor(1).getValue() == 133l;
-    assert LABELER.labelFor(2).getValue() == 1337l;
-    assert LABELER.labelFor(3).getValue() == 1337l;
+    assert LABELER.labelFor(1).getValue() == -1l;
+    assert LABELER.labelFor(2).getValue() == 133l;
+    assert LABELER.labelFor(3).getValue() == 133l;
     assert LABELER.labelFor(4).getValue() == 31337l;
   }
 
